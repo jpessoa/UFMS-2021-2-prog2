a3e453775ee87dd2d33b5e9748b458919db6b567
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-08-23 13:16:15 -0400

    Aula 01 2021-08-09

---
+++ Segundo_programa.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2, soma;
+    num1 = 25;
+    num2 = 30;
+    soma = num1 + num2;
+    printf("A soma de %d e %d ´e %d\n", num1, num2, soma);
+    return 0;
+}
