7aaa5f45c456db6dff66517d2cb172f999ea1054
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-08-25 19:03:02 -0400

    Aula de 2021/08/23

---
+++ 0823/ex3A.cpp
@@ -0,0 +1,33 @@
+/* Recebe duas datas (dd/mm/aaaa) e calcula o numero de dias decorridos entre elas*/
+#include <stdio.h>
+#include <math.h>   /* floor (x) - calcula o piso do número x, 
+						abs(x) - calcula o valor absoluto de x*/
+
+// definição de um tipo estrutura/registro/struct
+struct tipoData
+{
+	int dia, mes, ano;
+};
+
+
+int fAno(int ano, int mes)
+{
+
+}
+
+int gMes(int mes)
+{
+
+}
+
+int calculaN(tipoData data)
+{
+
+
+}
+
+int main()
+{
+
+	return 0;
+}
