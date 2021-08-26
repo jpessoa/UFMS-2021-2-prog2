f9d59ae9d706739190388e5b02642c8a8e7fb918
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-08-26 17:01:32 -0400

    Aula 1

---
+++ 0809/programa1.cpp
@@ -0,0 +1,19 @@
+/* 
+Aula de 2021-08-09
+*/
+#include <stdio.h> // Biblioteca de Entrada/Saída
+
+// Tipos de dados
+
+/*
+int    - número inteiros
+float  - números reais
+double - números reais com maior precisão
+void   - sem tipo
+*/
+
+int main()
+{
+    printf("Hello, world!\n");
+    return 0;
+}
