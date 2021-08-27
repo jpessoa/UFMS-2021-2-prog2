063421cde20a61331faf3fade42132553fe74a47
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-08-27 07:41:34 -0400

    .

--- Terceiro_programa.cpp
+++ Terceiro_programa.cpp
@@ -3,11 +3,11 @@
 int main()
 {
     int num1, num2, soma;
-    printf("Informe um n´umero: \n");
+    printf("Informe um número: \n");
     scanf("%d", &num1);
-    printf("Informe outro n´umero: \n");
+    printf("Informe outro número: \n");
     scanf("%d", &num2);
     soma = num1 + num2;
-    printf("A soma de %d mais %d ´e %d\n", num1, num2, soma);
+    printf("A soma de %d mais %d é %d\n", num1, num2, soma);
     return 0;
 }
