74944beafae105c6ad5d026e8c3f9d6206a01664
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-09-02 08:47:57 -0400

    mais commit

---
+++ 0901/ex11.2Aprof.cpp
@@ -0,0 +1,8 @@
+/*
+Solução da Professora
+Aula de 2021-09-01
+Exercício do Slide
+https://ava.ufms.br/pluginfile.php/625335/mod_resource/content/0/ponteiros-funcoes-slides-P04.pdf
+*/
+#include <stdio.h>
+#define MAX 100
