67972b0c7a937673bc0467c3884d45eacf953d6e
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-09-06 12:35:47 -0400

    20210903 2021-2 Prog2

---
+++ 0903/001.c
@@ -0,0 +1,8 @@
+/*
+Aula de 2021-09-03
+Exemplo do slide
+https://ava.ufms.br/pluginfile.php/625335/mod_resource/content/0/ponteiros-funcoes-slides-P04.pdf
+*/
+
+#include <stdio.h>
+#define MAX 100
