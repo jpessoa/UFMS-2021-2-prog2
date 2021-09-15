ed802bd1e233884e9da319b46c3d0dc70cc69783
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-09-15 18:41:29 -0400

    Wed Sep 15 18:41:02

---
+++ 0915/algumacoisa.cpp
@@ -0,0 +1,29 @@
+/*
+Aula de 2021-09-15
+https://ava.ufms.br/pluginfile.php/633348/mod_resource/content/0/algumacoisa.cpp
+*/
+#include <stdio.h>
+
+void imprime_alguma_coisa(int n);
+
+int main()
+{
+	int i;
+
+	for(i = 1; i <= 10; i++)
+	{
+		imprime_alguma_coisa(i);
+		printf("\n");
+	}
+
+	return 0;
+}
+
+void imprime_alguma_coisa(int n)
+{
+	if(n != 0)
+	{
+		imprime_alguma_coisa(n/2);
+		printf("%d", n%2);
+	}
+}
