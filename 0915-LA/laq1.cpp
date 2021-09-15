b80b190cfe3fba30975729fb8a3fb0c253cee8c1
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-09-15 10:58:38 -0400

    Wed Sep 15 10:58:22 -04 2021

---
+++ 0915-LA/laq1.cpp
@@ -0,0 +1,57 @@
+/*
+Lista avaliativa sobre registros (entrega até 15/09)
+https://ava.ufms.br/mod/quiz/view.php?id=320938
+
+Faça um programa para simular uma agenda de compromissos e:
+• Crie e leia um vetor de 5 estruturas de dados com: compromisso (máximo 60 letras) 
+  e data, nesta ordem. A data deve ser outra estrutura de dados contendo dia, mês e ano. 
+• Leia dois inteiros M e A e mostre todos os compromissos do mês M do ano A. Repita o 
+   procedimento até ler M = 0.
+
+Dica: utilize  scanf("%d/%d/%d", &variavel_dia, &variavel_mes, &variavel_ano); para 
+      leitura formatada da data.
+*/
+/*
+No link abaixo tem exemplod de agenda de aniversário
+https://ava.ufms.br/pluginfile.php/577638/mod_resource/content/1/reg-vet-mat.pdf
+*/
+
+#include <stdio.h>
+#include<string.h>
+#define MAX 60
+
+struct tipoData{
+    int dia, mes, ano;
+};
+
+struct Agenda
+    {
+        char compromisso[MAX];
+        tipoData datacompro;
+    };
+
+int main()
+{
+    int i, M, A, datacompro;
+    /* Crie e leia um vetor de 5 estruturas de dados com: compromisso (máximo 60 letras) 
+       e data, nesta ordem.       */
+    for (i = 0; i < 5; i++)
+    {
+        printf("Digite descrição do compromisso: ");
+        scanf(" %[^\n]", Agenda[i].compromisso);
+
+        printf("Digite a data no formato DD/MM/AAAA: ");
+        scanf("%d/%d/%d", &variavel_dia, &variavel_mes, &variavel_ano);
+
+    }
+
+    /* Leia dois inteiros M e A e mostre todos os compromissos do mês M do ano A. Repita o 
+       procedimento até ler M = 0.    */
+    while (M != 0)
+    {
+        printf("\nInforme uma data (MM/AAAA): ");
+        scanf("%d/%d", &datacompro.mes, &datacompro.ano);
+
+    }
+    
+}
