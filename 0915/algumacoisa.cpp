07acb6e2af04e5359ff48ae712ba0d3c7c8d0f4a
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-09-15 18:59:17 -0400

    Wed Sep 15 18:58:47

--- 0915/algumacoisa.cpp
+++ 0915/algumacoisa.cpp
@@ -9,13 +9,11 @@ void imprime_alguma_coisa(int n);
 int main()
 {
 	int i;
-
 	for(i = 1; i <= 10; i++)
 	{
 		imprime_alguma_coisa(i);
 		printf("\n");
 	}
-
 	return 0;
 }
 
@@ -24,6 +22,7 @@ void imprime_alguma_coisa(int n)
 	if(n != 0)
 	{
 		imprime_alguma_coisa(n/2);
-		printf("%d", n%2);
+		//printf("i=%d -> %d ", n, n%2);
+        printf("%d", n%2);
 	}
 }
---
+++ 0915/potencia.cpp
@@ -0,0 +1,17 @@
+/*
+Aula de 2021-09-15
+Copiar depois do vídeo da aula
+*/
+#include <stdio.h>
+
+int potenciaR(int x, int n)
+{
+
+
+}
+int main()
+{
+    int x, n;
+    scanf("%d%d", &x, &n);
+    printf("%d");
+}
