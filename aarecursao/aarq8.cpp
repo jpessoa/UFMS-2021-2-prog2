cc66a1d57d650d1f4be94fb6d72509284a2a668a
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-09-17 08:13:28 -0400

    Questão 8 Fri Sep 17 08:13:16

--- aarecursao/aarq8.cpp
+++ aarecursao/aarq8.cpp
@@ -2,11 +2,37 @@
 Lista avaliativa sobre recursão (entrega até 30/09)
 https://ava.ufms.br/mod/quiz/view.php?id=331022
 
+Considere a função recursiva ‘func’ definida por
+
+func(1) = 1
+func( n ) = (n - 1) * func(n - 1)
+
+Quais são os valores de func(4) e func(5), respectivamente?
 */
 
 #include <stdio.h>
-int func1(int x);
+int func(int n);
 
 int main()
 {
+    int o = 4, m;
+    m = func(o);
+    printf("\nfunc(4) = %d", m);
+    o = 5;
+    m = func(o);
+    printf("\nfunc(5) = %d\n", m);
+
+}
+
+
+int func(int n)
+{
+	if (n == 1)
+	{
+        return 1;
+	}
+    else
+    {
+        return (n - 1) * func(n - 1);
+    }
 }
