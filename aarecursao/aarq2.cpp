b848e7855a6bdeb5d18dcec360ef5b2ef1c3ec30
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-09-17 17:41:14 -0400

    Fri Sep 17 17:40:58

---
+++ aarecursao/aarq2.cpp
@@ -0,0 +1,47 @@
+/*
+Lista avaliativa sobre recursão (entrega até 30/09)
+https://ava.ufms.br/mod/quiz/view.php?id=331022
+
+Defina uma função recursiva conta que recebe como argumentos uma lista de n números inteiros armazenados em um 
+vetor W e um número inteiro k e devolve o número de vezes que k ocorre em W. A função deve ter o protótipo a seguir:
+
+int conta(int n, int *W, int k);
+
+Crie uma programa para ler um vetor com N números inteiros (N <= 20), além de um número inteiro k. 
+O programa deve imprimir o número de vezes que k aparece no vetor. Utilize a função recursiva conta no seu programa.
+
+Entrada:
+1
+20
+20
+Resultado esperado:
+1
+Resultado obtido:
+
+Entrada:
+7
+12 9 3 5 9 11 9
+9
+Resultado esperado:
+3
+Resultado obtido:
+
+*/
+#include <stdio.h>
+
+int conta(int n, int *W, int k);
+
+int main()
+{
+    int jjj;
+    printf("Hello world!");
+
+}
+
+int conta(int n, int *W, int k)
+{
+    int z;
+    // recebe como argumentos uma lista de n números inteiros armazenados em um 
+    // vetor W e um número inteiro k e devolve o número de vezes que k ocorre em W
+    return 0;
+}
