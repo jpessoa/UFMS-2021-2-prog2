04b377b4c8004be6f9b833452a1317386cb069e4
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-09-26 14:50:55 -0400

    Sun Sep 26 14:50:41

--- 0920/busca-sequencial.cpp
+++ 0920/busca-sequencial.cpp
@@ -1,12 +1,17 @@
 /*
 Aula de 2021-09-20
+Exemplo do slide :
+https://ava.ufms.br/pluginfile.php/634911/mod_resource/content/0/buscabin.pdf
+
+Recebe um número inteiro n >= 0, um vetor v[0..n-1] com n números inteiros e um número inteiro x e devolve k no intervalo
+[0, n-1] tal que v[k] == x. Se tal k não existe, devolve -1.
+
 
 */
 #include <stdio.h>
-#include<stdio.h>
 #define MAX 3
 
-int busca_sequencial(int n, int v[MAX], int x);
+int   busca_sequencial(int n, int v[MAX], int x);
 int busca_sequencial_R(int n, int v[MAX], int x);
 
 int main()
@@ -30,8 +35,8 @@ int busca_sequencial_R(int n, int v[MAX], int x)
     if (n == 0)
         return -1;
     else
-        if (x == v[n-1])
-            return -1;
+        if (x == v[n - 1])
+            return n - 1;
         else
-            return busca_sequencial_R(n -1, v, x);
+            return busca_sequencial_R(n - 1, v, x);
 }
---
+++ 0920/busca-vetor-ordenado.cpp
@@ -0,0 +1,47 @@
+/*
+Aula de 2021-09-20
+Exemplo do slide :
+https://ava.ufms.br/pluginfile.php/634911/mod_resource/content/0/buscabin.pdf
+
+Recebe um número inteiro n > 0, um vetor de números inteiros crescente v[0..n-1] e um número inteiro x 
+e devolve um índice k em [0, n] tal que v[k-1] < x <= v[k]
+
+*/
+#include <stdio.h>
+#define MAX 3
+
+int   busca_ordenada(int n, int v[MAX], int x);
+int    busca_binaria(int n, int v[MAX], int x);
+
+int main()
+{
+
+}
+// Busca em vetor ordenado 
+int busca_ordenada(int n, int v[MAX], int x)
+{
+    int k;
+
+    for (k = 0; k< n && v[k]< x; k++)
+        ;
+
+    return k; 
+
+}
+// Busca binária
+int busca_binaria(int n, int v[MAX], int x)
+{
+    int esq, dir, meio;
+
+    esq = -1;
+    dir = n;
+    while (esq < dir - 1) {
+        meio = (esq + dir) / 2;
+        if (v[meio] < x)
+            esq = meio;
+        else
+            dir = meio;
+    }
+    return dir;
+}
+
