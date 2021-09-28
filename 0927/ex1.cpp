1a37f3b6288345de55291e4e9a59c1f2ebf2320e
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-09-28 11:29:01 -0400

    testes

---
+++ 0927/ex1.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define MAX 3
+
+
+int main()
+{
+    int v[8] = {54,24,13,74,15,2,244,13}, *p;
+    p = v;
+
+
+
+}
---
+++ 0927/ex2.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define MAX 3
+
+
+int main()
+{
+
+
+}
