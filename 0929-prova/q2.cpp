1e098f8df7f4731b4a2c2522fb7f4c439a3937f7
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-09-29 20:48:28 -0400

    q1 q2 q3 0k faltando apenas q4

--- 0929-prova/q2.cpp
+++ 0929-prova/q2.cpp
@@ -7,30 +7,26 @@ int main()
 {
     int v[MAX];
     int n, k;
-
     // ler n
     //printf("Digite a quantidade de numeros a serem lidos : ");
 	scanf("%d", &n);
-    
     // ler k n vezes dentro de um laço para guardar no vetor v os k inteiros
     int i;
 	for(i = 0; i <= n-1; i++)
 	{
         scanf("\n%d", &v[i]);
 	}
-	
     // executar laço para ler n posições do vetor e executar a função recurvisa q2
 	for(i = 0; i <= n-1; i++)
 	{
         float S;
-        int p;
-        p = v[i];
-        S = q2(p);
-        //printf("\nEntrou no segundo laço");
-        printf("\nVI=%d  S=%.2f", v[i], S);
-        //printf("\n%.2f", S);
+        //int p;
+        //p = v[i];
+        //S = q2(p);
+        S = q2(v[i]);
+        //printf("\nVI=%d  S=%.2f", v[i], S);
+        printf("\n%.2f", S);
 	}
-
 }
 
 float q2(int k)
@@ -43,10 +39,9 @@ float q2(int k)
     }
     else 
     {
-        //aux = q2( k + ( (k^2-1) / k) ); 
-        aux2 = (k*k -1) / k ;
-        printf("\n              k*k -1 / k %f", aux2);
-        aux = q2(k-1) + ( (k*k-1) / k ) ; 
+        //aux2 = ((float)k*k -1) / k ;
+        //printf("\n              k*k -1 / k %f", aux2);
+        aux = q2(k-1) + ( (float)(k*k-1) / k ) ; 
     }
     return aux;
 }
---
+++ 0929-prova/q4.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define MAX 20
+
+struct tipoA{
+	char nome[MAX+1];
+	int op1, op2;
+};
+
+// PROTOTIPO DAS FUNCOES
+
+int main()
+{
+	int n;
+
+	// leitura de n
+	scanf("%d", &n);
+	// leitura do vetor
+
+	// chamada da funcao para calcular a quantidade de vezes da mais escolhida
+
+
+	// chamada da funcao para verificar aluno que informou as 2 optativas iguais
+
+
+	return 0;
+}
+
+
+// CORPO DAS FUNCOES
