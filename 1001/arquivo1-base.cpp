0336b42dfa44472e7437ee7f7073695061eb7334
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-10-01 19:35:51 -0400

    Fri Oct  1 19:35:34

---
+++ 1001/arquivo1-base.cpp
@@ -0,0 +1,59 @@
+/*
+ 
+ PROGRAMA PARA ABRIR UM ARQUIVO TEXTO CONTENDO VARIOS NUMEROS INTEIROS, 
+ LER OS NUMEROS DO ARQUIVO E IMPRIMI-LOS NA TELA
+ */
+
+#include<stdio.h>  /*FILE, printf, scanf*/
+#define MAX 30
+
+
+
+int main()
+{
+    FILE *arqent;
+    char nome[MAX];
+    int num;
+
+
+	printf("Nome do arquivo: ");
+    scanf(" %s", nome);
+    // abrir o arquivo
+    arqent = fopen(nome, "r");
+    if(arqent == NULL)
+    	printf("Erro na abertura do arquivo %s.", nome);
+    else{
+    	// ler os dados e processamento
+    	fscanf(arqent, "%d", &num);
+    	while( feof(arqent) == 0 )
+    	{
+    		printf("%d ", num);
+    		fscanf(arqent, "%d", &num);
+    	}
+
+    	// fechar o arquivo
+    	fclose(arqent);
+    }
+    
+    printf("\n");
+    return 0;
+}
+
+
+
+
+
+
+/*        fscanf(arqent, "% ", &num);
+        while( feof(arqent) == 0)   // chegou ao final do arquivo ?
+        {
+            ..... processamento
+
+            fscanf(arqent, "% ", );        
+        }   */ 
+
+
+
+
+
+
