ec54fc8472f27a11c7f199c96d99b233553fa195
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-10-16 15:35:17 -0400

    Sat Oct 16 15:34:58 -04 2021

--- 1016/uri-3089.cpp
+++ 1016/uri-3089.cpp
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-int main()
+void main()
 {
-    // Ler a quantidade de netos
     int n;
     scanf("%d", &n);
 
-    // Ler o valor dos presentes
     int qtdpres;
     qtdpres = 2*n;
     int v[qtdpres];
@@ -16,41 +14,17 @@ int main()
     int parum, pardois, meio, fim;
     fim = (qtdpres-1);
     meio = fim/2;
-    
     parum = v[0] + v[fim];
     pardois = v[meio] + v[meio+1]; 
 
-    //printf("\nP1 = %d // v0 = %d // vqtd = %d", parum, v[0] , v[fim]);
-    //printf("\nP2 = %d // meio = %d // meio+1 = %d\n\n", pardois,  v[meio],  v[meio+1]);
-
     if (parum > pardois)
     {
-        printf("%d %d", parum, pardois);
+        printf("%d %d\n", parum, pardois);
     }
     else
     {
-        printf("%d %d", pardois, parum);
+        printf("%d %d\n", pardois, parum);
     }
 
-    return 0;
+    //return 0;
 }
-
-
-/*
-    // Varrer o vetor buscando o mais caro e o mais barato
-    int caro, barato;
-    caro = 0;
-    barato = 1000000;
-    for (int i = 0; i < 2*n; i++)
-    {
-        if (v[i] > caro)
-            caro = v[i];
-        if (v[i] < barato)
-            barato = v[i];
-    }
-
-    printf("\nCaro = %d", caro);
-    printf("\nBarato = %d", barato);
-    printf("\nSoma = %d\n\n", caro+barato);
-
-*/
