ace65772a1735752e6225954626e4387058a42ec
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-11-11 12:53:02 -0400

    Thu Nov 11 12:52:53

--- 1112/qpilha.cpp
+++ 1112/qpilha.cpp
@@ -14,8 +14,23 @@ int main()
     P = NULL;
     printf("Hello World!\n");
     empilha(5,P);
-    
-
+    empilha(3,P);
+    desempilha(P); 
+    empilha(2,P); 
+    empilha(8,P); 
+    desempilha(P); 
+    desempilha(P); 
+    empilha(9,P); 
+    empilha(1,P); 
+    desempilha(P); 
+    empilha(7,P); 
+    empilha(6,P); 
+    desempilha(P); 
+    desempilha(P); 
+    empilha(4,P); 
+    desempilha(P); 
+    desempilha(P); 
+    empilha(13,P);
   return 0;
 }
 
@@ -28,23 +43,24 @@ Qual o conteúdo de uma pilha P inicialmente vazia após a execução das seguin
 */
 void empilha(int x, celula *&lst)
 {
- celula *nova;
- nova = (celula *) malloc(sizeof (celula));
- nova->chave = x;
- nova->prox = lst;
- lst = nova;
+    celula *nova;
+    nova = (celula *) malloc(sizeof (celula));
+    nova->chave = x;
+    nova->prox = lst;
+    lst = nova;
 }
 int desempilha(celula *&lst)
 {
- celula *aux;
- int num;
- if(lst == NULL)
- return 0;
- else{
- num = lst->chave;
- aux = lst;
- lst = lst->prox;
- free(aux);
- return num;
- }
+    celula *aux;
+    int num;
+    if(lst == NULL)
+        return 0;
+    else
+    {
+        num = lst->chave;
+        aux = lst;
+        lst = lst->prox;
+        free(aux);
+        return num;
+    }
 }
