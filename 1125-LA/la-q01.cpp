3ba5c8e476f82229a803596a8182dad33e39f0ff
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-11-22 14:34:28 -0400

    Mon Nov 22 14:34:02

---
+++ 1125-LA/la-q01.cpp
@@ -0,0 +1,16 @@
+
+
+/*
+Escreva uma função que conte o número de células de uma lista linear encadeada.
+
+Entrada: A entrada é composta dos valores a inserir na lista na ordem em que aparecem, sendo que o número 0 finaliza a entrada.
+
+1 5 8 7 23 98 42 31 14 33 0
+
+Saída: A saída representa o número de células da lista.
+
+10
+
+Crie um programa para testar sua função respeitando o formato da entrada e da saída.
+
+*/
