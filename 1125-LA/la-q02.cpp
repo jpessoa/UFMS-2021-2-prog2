4d9e040d7956b5f687a15d28a1706c04e6b822d4
Jefferson E. Pessoa - Easy <jefferson@mile.com.br>
2021-11-24 08:47:50 -0400

    Wed Nov 24 08:47:28

---
+++ 1125-LA/la-q02.cpp
@@ -0,0 +1,24 @@
+/*
+Considerando o tipo struct definido abaixo,
+
+struct celula{
+
+     int valor;
+
+     struct celula *prox;
+
+};
+
+escreva uma função soma para retornar a soma dos elementos de uma lista L.
+
+int soma(celula *L);
+
+Entrada: Uma sequência de números inteiros. (Uma entrada negativa encerra a entrada).
+
+1 5 8 7 23 98 42 31 14 33 -1
+
+Saída: A saída representa a soma dos elementos da lista lista.
+
+262
+
+*/
---
+++ 1125-LA/la-q03.cpp
@@ -0,0 +1,13 @@
+/*
+Utilizando uma pilha, escreva uma função que receba um número inteiro positivo no formato decimal e converte este número para o formato binário. A função retorna uma lista encadeada (a pilha) com os dígitos que fazem parte do número em binário. Ao fim imprima a lista resultante.
+
+Entrada: A entrada é composta do valor em decimal.
+
+5  
+
+Saída: A saída é a impressão da pilha com os dígitos binários.
+
+101
+
+Crie um programa para testar sua função respeitando o formato da entrada e saída.
+*/
